5b140b15dfa874a28ba4ba32df9e1386a104c1a7
claretnnamocha <devclareo@gmail.com>
2022-01-04 13:19:58 +0100

    better error code handling

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -1,19 +1,23 @@
 import { Router } from "express";
-import { authenticate, validate } from "../../middlewares";
+import { authenticate, controller, validate } from "../../middlewares";
 import * as user from "./controller";
 import * as validator from "./validators";
 
 const routes = Router();
 routes.use(authenticate);
 
-routes.get("", user.getProfile);
+routes.get("", controller(user.getProfile));
 
 routes.put(
   "/change-password",
   validate(validator.changePassword),
-  user.changePassword
+  controller(user.changePassword)
 );
 
-routes.put("/edit-profile", validate(validator.editProfile), user.editProfile);
+routes.put(
+  "/edit-profile",
+  validate(validator.editProfile),
+  controller(user.editProfile)
+);
 
 export default routes;
