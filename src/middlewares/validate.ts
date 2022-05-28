65924db4728e99975addceb028465a1124e26b14
claretnnamocha <devclareo@gmail.com>
2022-05-28 13:38:51 +0100

    lint

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -3,36 +3,35 @@ import Joi from "joi";
 import { response } from "../helpers";
 import { CustomRequest } from "../types/controllers";
 
-export const validate =
-  (obj: any) => (req: CustomRequest, res: Response, next: NextFunction) => {
-    const schema = Joi.object().keys(obj).required().unknown(false);
-    const payload = req.method === "GET" ? req.query : req.body;
-    const { params } = req;
-    const value = { ...params, ...payload };
+export const validate = (obj: any) => (req: CustomRequest, res: Response, next: NextFunction) => {
+  const schema = Joi.object().keys(obj).required().unknown(false);
+  const payload = req.method === "GET" ? req.query : req.body;
+  const { params } = req;
+  const value = { ...params, ...payload };
 
-    const [duplicateField] = Object.keys(payload).filter(
-      {}.hasOwnProperty.bind(params)
+  const [duplicateField] = Object.keys(payload).filter(
+    {}.hasOwnProperty.bind(params),
+  );
+
+  if (duplicateField) {
+    return response(
+      res,
+      {
+        status: false,
+        message: `Duplicate  field '${duplicateField}' present in url params and ${
+          req.method === "GET" ? "query" : "body"
+        }`,
+      },
+      422,
     );
+  }
 
-    if (duplicateField)
-      return response(
-        res,
-        {
-          status: false,
-          message: `Duplicate  field '${duplicateField}' present in url params and ${
-            req.method === "GET" ? "query" : "body"
-          }`,
-        },
-        422
-      );
+  const { error, value: vars } = schema.validate(value);
 
-    const { error, value: vars } = schema.validate(value);
+  if (error) return response(res, { status: false, message: error.message }, 422);
 
-    if (error)
-      return response(res, { status: false, message: error.message }, 422);
+  req.form = req.form || {};
+  req.form = { ...req.form, ...vars };
 
-    req.form = req.form || {};
-    req.form = { ...req.form, ...vars };
-
-    return next();
-  };
+  return next();
+};
